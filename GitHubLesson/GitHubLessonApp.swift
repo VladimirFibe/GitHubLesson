4f77076f4a06240a9f20603b70435a93cae3c006
Vladimir Fibe <motiw@icloud.com>
2022-11-01 21:23:14 +0600

    Initial Commit

---
+++ GitHubLesson/GitHubLessonApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubLessonApp.swift
+//  GitHubLesson
+//
+//  Created by Vladimir Fibe on 01.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct GitHubLessonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
